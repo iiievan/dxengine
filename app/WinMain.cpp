e167b9e7734e2bf8a9c219ccc53b3fd04534e8e4
WIN-VNEPR2L7SRH\ishopov <mbo.kastnev@gmail.com>
2025-08-13 01:44:07 +0300

    [Window Framework] 6 https://www.youtube.com/watch?v=D-PC-huX-l8&list=PLqCJpWy5Fohd3S7ICFXwUomYW0Wv67pDD&index=7

--- app/WinMain.cpp
+++ app/WinMain.cpp
@@ -1,46 +1,11 @@
 #include <windows.h>
-#include <iostream>
-#include <sstream>
-
 #include "Utils.hpp"
-
-static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+#include "Window.h"
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-    const auto pAppName = "DxEngine";
-
-    // register window class
-    WNDCLASSEX wc = {0};
-    wc.cbSize = sizeof(wc);
-    wc.style = CS_OWNDC;
-    wc.lpfnWndProc = WndProc;
-    wc.cbClsExtra = 0;
-    wc.hInstance = hInstance;
-    wc.hIcon = nullptr;
-    wc.hCursor = nullptr;
-    wc.hbrBackground = nullptr;
-    wc.lpszMenuName = nullptr;
-    wc.lpszClassName = pAppName;
-    wc.hIconSm = nullptr;
-
-    RegisterClassEx(&wc);
-
-    // create window instance
-    HWND hwnd = CreateWindowEx(0,
-                               pAppName,
-                               "Engine main window.",
-                               WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
-                               200,
-                               200,
-                               640,
-                               480,
-                               nullptr,
-                               nullptr,
-                               hInstance,
-                               nullptr);
-
-    ShowWindow(hwnd, SW_SHOW);
+    Window wnd(800, 600, "DxEngine");
+    Window wnd2(640, 480, "SetUp");
 
     MSG msg;
     BOOL gResult;
@@ -56,39 +21,3 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
     return static_cast<int>(msg.wParam);
 }
 
-static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-    static WindowsMessageMap mm;
-    DebugPrint( mm( msg,lParam,wParam ).c_str() );
-
-    switch (msg)
-    {
-        case WM_CLOSE:
-            PostQuitMessage(69);
-            break;
-        case WM_KEYDOWN:
-            if (wParam == 'F')
-                SetWindowText(hwnd,"Hola, Amigo:)");
-            break;
-        case WM_KEYUP:
-            if (wParam == 'F')
-                SetWindowText(hwnd,"I'am an App!");
-            break;
-        case WM_CHAR:
-        {
-            static std::string title;
-            title.push_back((char)wParam);
-            SetWindowText(hwnd,title.c_str());
-        }
-        break;
-        case WM_LBUTTONDOWN:
-        {
-            POINTS pt = MAKEPOINTS(lParam);
-            std::ostringstream oss;
-            oss << "(" << pt.x << ", " << pt.y << ")";
-            SetWindowText(hwnd,oss.str().c_str());
-        }
-        break;
-    }
-    return DefWindowProc(hwnd, msg, wParam, lParam);
-}
