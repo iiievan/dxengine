fc80260a9686c9847e6b714f8d315834829ef2c0
WIN-VNEPR2L7SRH\ishopov <mbo.kastnev@gmail.com>
2025-08-20 02:05:19 +0300

    [First Triangle Pt 2] 17 https://www.youtube.com/watch?v=KR8bP0G07fc&list=PLqCJpWy5Fohd3S7ICFXwUomYW0Wv67pDD&index=18

--- graphics/Graphics.cpp
+++ graphics/Graphics.cpp
@@ -1,33 +1,47 @@
 #include "Graphics.h"
+#include <d3dcompiler.h>
 #include <sstream>
 #include "Utils.hpp"
 #include "dxerr.h"
-#include <d3dcompiler.h>
 
-#pragma comment(lib,"d3d11.lib")
+#pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "d3dcompiler.lib")
 
 namespace wrl = Microsoft::WRL;
 
 // graphics exception checking/throwing macros (some with dxgi infos)
-#define GFX_EXCEPT_NOINFO(hr) Graphics::HrException( __LINE__,__FILE__,(hr) )
-#define GFX_THROW_NOINFO(hrcall) if( FAILED( hr = (hrcall) ) ) throw Graphics::HrException( __LINE__,__FILE__,hr )
+#define GFX_EXCEPT_NOINFO(hr) Graphics::HrException(__LINE__, __FILE__, (hr))
+#define GFX_THROW_NOINFO(hrcall) \
+    if (FAILED(hr = (hrcall)))   \
+    throw Graphics::HrException(__LINE__, __FILE__, hr)
 
 #ifndef NDEBUG
-#define GFX_EXCEPT(hr) Graphics::HrException( __LINE__,__FILE__,(hr),infoManager.GetMessages() )
-#define GFX_THROW_INFO(hrcall) infoManager.Set(); if( FAILED( hr = (hrcall) ) ) throw GFX_EXCEPT(hr)
-#define GFX_DEVICE_REMOVED_EXCEPT(hr) Graphics::DeviceRemovedException( __LINE__,__FILE__,(hr),infoManager.GetMessages() )
-#define GFX_THROW_INFO_ONLY(call) infoManager.Set(); (call); {auto v = infoManager.GetMessages(); if(!v.empty()) {throw Graphics::InfoException( __LINE__,__FILE__,v);}}
+#define GFX_EXCEPT(hr) Graphics::HrException(__LINE__, __FILE__, (hr), infoManager.GetMessages())
+#define GFX_THROW_INFO(hrcall) \
+    infoManager.Set();         \
+    if (FAILED(hr = (hrcall))) \
+    throw GFX_EXCEPT(hr)
+#define GFX_DEVICE_REMOVED_EXCEPT(hr) \
+    Graphics::DeviceRemovedException(__LINE__, __FILE__, (hr), infoManager.GetMessages())
+#define GFX_THROW_INFO_ONLY(call)                                 \
+    infoManager.Set();                                            \
+    (call);                                                       \
+    {                                                             \
+        auto v = infoManager.GetMessages();                       \
+        if (!v.empty())                                           \
+        {                                                         \
+            throw Graphics::InfoException(__LINE__, __FILE__, v); \
+        }                                                         \
+    }
 #else
-#define GFX_EXCEPT(hr) Graphics::HrException( __LINE__,__FILE__,(hr) )
+#define GFX_EXCEPT(hr) Graphics::HrException(__LINE__, __FILE__, (hr))
 #define GFX_THROW_INFO(hrcall) GFX_THROW_NOINFO(hrcall)
-#define GFX_DEVICE_REMOVED_EXCEPT(hr) Graphics::DeviceRemovedException( __LINE__,__FILE__,(hr) )
+#define GFX_DEVICE_REMOVED_EXCEPT(hr) Graphics::DeviceRemovedException(__LINE__, __FILE__, (hr))
 #define GFX_THROW_INFO_ONLY(call) (call)
 #endif
 
-
 Graphics::HrException::HrException(int line, const char *file, HRESULT hr, std::vector<std::string> infoMsgs) noexcept
-: Exception(line, file), m_hr(hr)
+    : Exception(line, file), m_hr(hr)
 {
     for (const auto &m : infoMsgs)
     {
@@ -44,11 +58,11 @@ const char *Graphics::HrException::what() const noexcept
 {
     std::ostringstream oss;
     oss << GetType() << std::endl
-        << "[Error Code] 0x" << std::hex << std::uppercase << GetErrorCode()
-        << std::dec << " (" << (unsigned long)GetErrorCode() << ")" << std::endl
+        << "[Error Code] 0x" << std::hex << std::uppercase << GetErrorCode() << std::dec << " ("
+        << (unsigned long)GetErrorCode() << ")" << std::endl
         << "[Error String] " << GetErrorString() << std::endl
         << "[Description] " << GetErrorDescription() << std::endl;
-    if( !m_info.empty() )
+    if (!m_info.empty())
     {
         oss << "\n[Error Info]\n" << GetErrorInfo() << std::endl << std::endl;
     }
@@ -90,8 +104,7 @@ Graphics::InfoException::InfoException(int line, const char *file, std::vector<s
 const char *Graphics::InfoException::what() const noexcept
 {
     std::ostringstream oss;
-    oss << GetType() << std::endl
-        << "\n[Error Info]\n" << GetErrorInfo() << std::endl << std::endl;
+    oss << GetType() << std::endl << "\n[Error Info]\n" << GetErrorInfo() << std::endl << std::endl;
     oss << GetOriginString();
     m_whatBuffer = oss.str();
     return m_whatBuffer.c_str();
@@ -120,7 +133,6 @@ Graphics::Graphics(HWND hWnd)
     swchd.Windowed = TRUE;                                                    //  no fullscreen
     swchd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;                              //
     swchd.Flags = 0;                                                          //
-
 
     UINT swapCreateFlags = 0u;
 #ifndef NDEBUG
@@ -156,6 +168,7 @@ void Graphics::EndFrame()
     infoManager.Set();
 #endif
 
+    // present Back Buffer on the screen
     if (FAILED(hr = m_pSwapChain->Present(1u, 0u)))
     {
         if (hr == DXGI_ERROR_DEVICE_REMOVED)
@@ -180,12 +193,7 @@ void Graphics::DrawTestTriangle()
         float y;
     };
 
-    const Vertex vertices[] =
-    {
-        {0.0f,0.5f},
-        {0.5f,-0.5f},
-        {-0.5f,-0.5f}
-    };
+    const Vertex              vertices[] = {{0.0f, 0.5f}, {0.5f, -0.5f}, {-0.5f, -0.5f}};
     wrl::ComPtr<ID3D11Buffer> pVertexBuffer;
 
     D3D11_BUFFER_DESC bd = {};
@@ -198,21 +206,58 @@ void Graphics::DrawTestTriangle()
     sd.pSysMem = vertices;
 
     HRESULT hr;
-    GFX_THROW_INFO(m_pDevice->CreateBuffer(&bd,&sd,&pVertexBuffer));
+    GFX_THROW_INFO(m_pDevice->CreateBuffer(&bd, &sd, &pVertexBuffer));
 
     const UINT stride = sizeof(Vertex);
     const UINT offset = 0u;
 
-    m_pContext->IASetVertexBuffers(0u,1u,&pVertexBuffer,&stride,&offset);
+    m_pContext->IASetVertexBuffers(0u, 1u, pVertexBuffer.GetAddressOf(), &stride, &offset);
+
+    // create pixel shader
+    wrl::ComPtr<ID3DBlob>           pBlob;
+    wrl::ComPtr<ID3D11PixelShader> pPixelShader;
+    GFX_THROW_INFO(D3DReadFileToBlob(L"shaders/Triangle.ps.cso", &pBlob)); // set path in cmake-build-debug folder
+    GFX_THROW_INFO(
+        m_pDevice->CreatePixelShader(pBlob->GetBufferPointer(), pBlob->GetBufferSize(), nullptr, &pPixelShader));
+
+    // bind pixel shader
+    m_pContext->PSSetShader(pPixelShader.Get(), nullptr, 0);
 
     // create vertex shader
     wrl::ComPtr<ID3D11VertexShader> pVertexShader;
-    wrl::ComPtr<ID3DBlob> pBlob;
-    GFX_THROW_INFO(D3DReadFileToBlob(L"shaders/Triangle.vs.cso",&pBlob));
-    GFX_THROW_INFO(m_pDevice->CreateVertexShader(pBlob->GetBufferPointer(),pBlob->GetBufferSize(),nullptr,&pVertexShader));
+    GFX_THROW_INFO(D3DReadFileToBlob(L"shaders/Triangle.vs.cso", &pBlob)); // set path in cmake-build-debug folder
+    GFX_THROW_INFO(
+        m_pDevice->CreateVertexShader(pBlob->GetBufferPointer(), pBlob->GetBufferSize(), nullptr, &pVertexShader));
 
     // bind vertex shader
-    m_pContext->VSSetShader(pVertexShader.Get(),nullptr,0);
+    m_pContext->VSSetShader(pVertexShader.Get(), nullptr, 0);
+
+    wrl::ComPtr<ID3D11InputLayout> pIL;
+    const D3D11_INPUT_ELEMENT_DESC ied[] = {
+        {"Position", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
+    };
+    GFX_THROW_INFO(m_pDevice->CreateInputLayout(
+        ied, (UINT)std::size(ied), pBlob->GetBufferPointer(), pBlob->GetBufferSize(), &pIL));
+
+    // bind vertex layout
+    m_pContext->IASetInputLayout(pIL.Get());
+
+    // set pixels of Back Buffer
+    m_pContext->OMSetRenderTargets(1u, m_pTargetView.GetAddressOf(), nullptr);
+
+    m_pContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+    //  Настраивается Viewport — область назначения для отрисовки. Здесь мы говорим:
+    //  "Рисуй на всю цель рендера (800x600), преобразовывая глубину из диапазона [0,1] в этот же диапазон".
+    // Это преобразует координаты из NDC (-1 до 1) в координаты экрана (0 до 800, 0 до 600).
+    D3D11_VIEWPORT vp = {};
+    vp.Width = 800;
+    vp.Height = 600;
+    vp.MinDepth = 0;
+    vp.MaxDepth = 1;
+    vp.TopLeftX = 0;
+    vp.TopLeftY = 0;
+    m_pContext->RSSetViewports(1u, &vp);
 
     GFX_THROW_INFO_ONLY(m_pContext->Draw((UINT)std::size(vertices), 0u));
 }
