6e7f7d63f07e0e9f9b49ae6922918f9352939255
WIN-VNEPR2L7SRH\ishopov <mbo.kastnev@gmail.com>
2025-10-06 23:26:01 +0300

    working as expected in video

--- shaders/PhongNormalMap.ps.hlsl
+++ shaders/PhongNormalMap.ps.hlsl
@@ -36,7 +36,7 @@ float4 PSMain(float3 worldPos : Position, float3 n : Normal, float3 tan : Tangen
 		n.x = normalSample.x * 2.0f - 1.0f;
 		n.y = -normalSample.y * 2.0f + 1.0f;  // flip y_component because briks normal map is for opengl
         									  // for opengl this is same as for x_component: normalSample.y * 2.0f - 1.0f;
-		n.z = normalSample.z;
+		n.z = -normalSample.z;
 
         // bring normal from tanspace into view space
         n = mul(n, tanToView);
